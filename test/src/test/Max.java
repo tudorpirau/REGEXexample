6e1c11816be22328217d4d0082f34c5cbe52901c
tudorpirau <tudor.pirau@gmail.com>
2019-11-24 13:15:16 +0200

    Adj 2.

--- test/src/test/Max.java
+++ test/src/test/Max.java
@@ -8,19 +8,5 @@ public class Max extends Expression{
 		this.exp=exp;
 		this.type=type;
 		}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public String getExp() {
-		return exp;
-	}
-	public void setExp(String exp) {
-		this.exp = exp;
-	}
-	public void print() {
-		System.out.println(type+" : "+exp);
-    }
+
 }
--- test/src/test/Operation.java
+++ test/src/test/Operation.java
@@ -8,20 +8,6 @@ public Operation (String exp, String type){
 	this.exp=exp;
 	this.type=type;
 	}
-public String getType() {
-	return type;
-}
-public void setType(String type) {
-	this.type = type;
-}
-public String getExp() {
-	return exp;
-}
-public void setExp(String exp) {
-	this.exp = exp;
-}
-public void print() {
-	System.out.println(type+" : "+exp);
-}
+
 }
 
